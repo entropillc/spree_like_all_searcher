e9738b0748578d57cb6147f49cd2b0c0b2402c9c
Nicholas Watson <nwwatson@gmail.com>
2012-05-15 15:53:07 -0400

    update to engine initializer

--- lib/spree_like_all_searcher/engine.rb
+++ lib/spree_like_all_searcher/engine.rb
@@ -1,17 +1,20 @@
+require 'spree_core'
+
 module SpreeLikeAllSearcher
   class Engine < Rails::Engine
     engine_name 'spree_like_all_searcher'
-    
-    initializer "spree.spree_like_all_searcher.preferences", :after => "spree.environment" do |app|
+
+    initializer "spree.search.preferences", :after => "spree.environment" do |app|
       Spree::Config.searcher_class = SpreeLikeAllSearcher::Base
     end
-    
+
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
-    config.autoload_paths += %W(#{config.root}/lib)
+    
     config.to_prepare &method(:activate).to_proc
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
